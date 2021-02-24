fc74b9e5bc98f5fe15f246fffb65a44644f81590
kamatuszewski <s16704@pjwstk.edu.pl>
2021-02-24 20:51:42 +0100

    generate project

---
+++ src/environments/environment.dev.ts
@@ -0,0 +1,8 @@
+import { IEnvironment } from './environment.interface';
+
+export const environment: IEnvironment = {
+  production: false,
+  apiUrl: {
+    auth: 'http://localhost:8080/api/v1/auth'
+  }
+};
