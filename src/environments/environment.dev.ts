9f951c24236f34175ce00825b9e5a69b4bed9661
kamatuszewski <s16704@pjwstk.edu.pl>
2021-08-10 15:36:14 +0200

    preparation of connection with be

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -3,8 +3,8 @@ import { IEnvironment } from './environment.interface';
 export const environment: IEnvironment = {
   production: false,
   apiUrl: {
-    auth: 'http://localhost:8600/api/v1/auth',
-    animals: 'https://0b0bdce9-0be1-4cdd-b034-fd5752a61915.mock.pstmn.io/api/animals',
-    dictionaries: 'https://0b0bdce9-0be1-4cdd-b034-fd5752a61915.mock.pstmn.io/api/dictionaries'
+    auth: 'api/Auth',
+    animals: 'api/Animals',
+    dictionaries: 'api/Dictionaries'
   }
 };
