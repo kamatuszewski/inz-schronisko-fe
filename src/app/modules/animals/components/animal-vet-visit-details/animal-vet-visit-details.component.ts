96c815b6f9012a18994deff95b311fd431757df0
kamatuszewski <s16704@pjwstk.edu.pl>
2021-08-16 23:53:50 +0200

    create vet visit details part2 and remove vet visit

--- src/app/modules/animals/components/animal-vet-visit-details/animal-vet-visit-details.component.ts
+++ src/app/modules/animals/components/animal-vet-visit-details/animal-vet-visit-details.component.ts
@@ -1,19 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import { of, Observable } from 'rxjs';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { ActivatedRoute, Router } from '@angular/router';
+import { of, Observable, Subject } from 'rxjs';
+import { takeUntil } from 'rxjs/operators';
+import { CoreService } from '../../../core/core.service';
+import { ConfirmDecisionModalService } from '../../../shared/services/confirm-decision-modal.service';
+import { AnimalsService } from '../../animals.service';
+import { IVetVisits } from '../../interfaces/animals.interface';
 
 @Component({
   selector: 'app-animal-vet-visit-details',
   templateUrl: './animal-vet-visit-details.component.html',
   styleUrls: ['./animal-vet-visit-details.component.scss']
 })
-export class AnimalVetVisitDetailsComponent implements OnInit {
+export class AnimalVetVisitDetailsComponent implements OnInit, OnDestroy {
+  public data$: Observable<IVetVisits>;
+  private onDestroy$ = new Subject<void>();
+  private vetVisitId: number;
+  private animalId: number;
 
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute,
+              private animalService: AnimalsService,
+              private confirmModal: ConfirmDecisionModalService,
+              private coreService: CoreService,
+              private router: Router
+  ) {
+    this.vetVisitId = activatedRoute.snapshot.params.vetVisitId;
+    this.animalId = activatedRoute.snapshot.params.id;
+  }
 
-  ngOnInit(): void {
+  public edit(): void {
+
+  }
+
+  public ngOnDestroy(): void {
+    this.onDestroy$.next();
+    this.onDestroy$.complete();
+  }
+
+  public ngOnInit(): void {
+    this.loadData();
   }
 
   public ofObservable<T>(data: T[]): Observable<any> {
     return of(data);
+  }
+
+  public remove(): void {
+    const data = {
+      description: 'VET_VISIT.DETAILS.CONFIRMATION_REMOVE',
+      ...ConfirmDecisionModalService.defaultActionConfig()
+    }
+    this.confirmModal.openDialog(data)
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe(this.removeVisit)
+  }
+
+  private loadData(): void {
+    if (!!this.vetVisitId) {
+      this.data$ = this.animalService.getVetVisits({id: this.vetVisitId});
+    }
+  }
+
+  private removeVisit = (): void => {
+    this.animalService.removeVetVisit({id: this.vetVisitId})
+      .pipe(takeUntil(this.onDestroy$))
+      .subscribe(
+        () => {
+          this.coreService.showSuccessMessage('VET_VISIT.DETAILS.MESSAGES.SUCCESS');
+          this.router.navigate(['animals', this.animalId]).then();
+        },
+        () => this.coreService.showSuccessMessage('VET_VISIT.DETAILS.MESSAGES.ERROR')
+      )
   }
 }
--- src/app/modules/animals/interfaces/animals.interface.ts
+++ src/app/modules/animals/interfaces/animals.interface.ts
@@ -1,7 +1,9 @@
 import { ESex } from '../../shared/enums/sex.enum';
 import { IGenericDictionary } from '../../shared/interfaces/generic.interface';
+import { IVetSpecialty } from '../../users/interfaces/user.interface';
 
 export interface ISimpleAnimal {
+  birthDate: string;
   id: number;
   name: string;
   sex: string;
@@ -111,7 +113,19 @@ export interface ITreatments {
 }
 
 export interface IVetVisits {
+  animal: ISimpleAnimal;
+  description: string;
   id: number;
   medicines: IMedicines[];
-  treatments: ITreatments[]
+  treatments: ITreatments[];
+  vet: ISimpleVet;
+  visitDate: string;
+}
+
+export interface ISimpleVet {
+  firstName: string;
+  id: number;
+  lastName: string;
+  sex: ESex;
+  specialties: IVetSpecialty[];
 }
